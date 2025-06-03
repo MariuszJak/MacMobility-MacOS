c5b915506c0a66c9b98eea2048637a9e8875a3e7
MariuszJak <mjakowienko87@gmail.com>
2025-06-03 19:52:26 +0200

    Added initializer for decoder.

--- MacMobility-MacOS/Shortcuts/ShortcutObject.swift
+++ MacMobility-MacOS/Shortcuts/ShortcutObject.swift
@@ -48,7 +48,7 @@ public struct ShortcutObject: Identifiable, Codable, Equatable {
         page: Int,
         index: Int? = nil,
         indexes: [Int]? = nil,
-        size: CGSize? = .init(width: 1, height: 1),
+        size: CGSize? = nil,
         path: String? = nil,
         id: String,
         title: String,
@@ -69,7 +69,7 @@ public struct ShortcutObject: Identifiable, Codable, Equatable {
         self.path = path
         self.id = id
         self.title = title
-        self.size = size
+        self.size = size ?? .init(width: 1, height: 1)
         self.color = color
         self.scriptCode = scriptCode
         self.utilityType = utilityType
@@ -79,5 +79,26 @@ public struct ShortcutObject: Identifiable, Codable, Equatable {
         self.objects = objects
         self.showTitleOnIcon = showTitleOnIcon
         self.category = category
+    }
+    
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.index = try container.decodeIfPresent(Int.self, forKey: .index)
+        self.indexes = try container.decodeIfPresent([Int].self, forKey: .indexes) ?? [self.index ?? 0]
+        self.page = try container.decode(Int.self, forKey: .page)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.path = try container.decodeIfPresent(String.self, forKey: .path)
+        self.color = try container.decodeIfPresent(String.self, forKey: .color)
+        self.faviconLink = try container.decodeIfPresent(String.self, forKey: .faviconLink)
+        self.type = try container.decode(ShortcutType.self, forKey: .type)
+        self.imageData = try container.decodeIfPresent(Data.self, forKey: .imageData)
+        self.browser = try container.decodeIfPresent(Browsers.self, forKey: .browser)
+        self.scriptCode = try container.decodeIfPresent(String.self, forKey: .scriptCode)
+        self.utilityType = try container.decodeIfPresent(UtilityObject.UtilityType.self, forKey: .utilityType)
+        self.objects = try container.decodeIfPresent([ShortcutObject].self, forKey: .objects)
+        self.showTitleOnIcon = try container.decodeIfPresent(Bool.self, forKey: .showTitleOnIcon)
+        self.category = try container.decodeIfPresent(String.self, forKey: .category)
+        self.size = try container.decodeIfPresent(CGSize.self, forKey: .size)
     }
 }
